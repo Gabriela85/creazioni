a7896b382676feb7f91e91589a7a0de0457122ca
Gabriela85 <77332290+Gabriela85@users.noreply.github.com>
2021-04-26 22:58:03 +0200

    fixing mobile media query

--- src/index.js
+++ src/index.js
@@ -27,3 +27,14 @@ function complete(){
     timer=null;
 }
 
+//nav bar//
+
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".menu");
+
+    burger.addEventListener("click", () => {
+     nav.classList.toggle("nav-active");
+    });
+}
+navSlide();
