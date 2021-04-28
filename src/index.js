8ad98f2eaaeeef7768f42b50fe6bdd555848e244
Gabriela85 <77332290+Gabriela85@users.noreply.github.com>
2021-04-28 23:22:39 +0200

    burger animation bug partially fixed
    still not clear why this part does not work on the separated list

--- src/index.js
+++ src/index.js
@@ -32,9 +32,26 @@ function complete(){
 const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".menu");
+    const navLinks = document.querySelectorAll(".menu li");
 
     burger.addEventListener("click", () => {
+        //toggle nav
+
      nav.classList.toggle("nav-active");
+
+    //animation links
+    navLinks.forEach((link, index) =>{
+if (link.style.animation) {
+    link.style.animation = ""
+} else{
+link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.5}s`;
+
+}
     });
+
+    //burger animation
+burger.classList.toggle("animate");
+
+        });
 }
 navSlide();
