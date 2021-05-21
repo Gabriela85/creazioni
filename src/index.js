a4c6ee60372b1e15f909db750c479bc420c68b08
Gabriela85 <77332290+Gabriela85@users.noreply.github.com>
2021-05-21 22:54:15 +0200

    created a choco version

--- src/index.js
+++ src/index.js
@@ -1,30 +1,7 @@
-//header in motion//
-
-const text = document.querySelector(".main");
-const strText = text.textContent;
-const splitText = strText.split("");
-text.textContent= "";
-
-for(let i=0; i< splitText.length; i++){
-    text.innerHTML += "<em>"+ splitText[i] + "</em>";
-}
-
-let char = 0;
-let timer = setInterval(onTick, 50);
-
-function onTick(){
-    const em = text.querySelectorAll("em")[char];
-    em.classList.add("fade");
-    char++
-    if(char===splitText.length){
-complete ();
-return;
-    }
-}
-
-function complete(){
-    clearInterval(timer);
-    timer=null;
+//logo animation
+const logo = document.querySelectorAll("#logo path");
+for(let i = 0; i<logo.length; i++){
+    console.log(`letter ${i} is ${logo [i].getTotalLength()}`);
 }
 
 //nav bar//
