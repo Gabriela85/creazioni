0ac4ed3b9923e9b39dae78cba2b4e989e8cf7e67
Gabriela85 <77332290+Gabriela85@users.noreply.github.com>
2021-05-24 23:51:55 +0200

    cookies plugin

--- src/glowCookies.js
+++ src/glowCookies.js
@@ -1,39 +1,3 @@
-//logo animation
-const logo = document.querySelectorAll("#logo path");
-for(let i = 0; i<logo.length; i++){
-    console.log(`letter ${i} is ${logo [i].getTotalLength()}`);
-}
-
-//nav bar//
-
-const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".menu");
-    const navLinks = document.querySelectorAll(".menu li");
-
-    burger.addEventListener("click", () => {
-        //toggle nav
-
-     nav.classList.toggle("nav-active");
-
-    //animation links
-    navLinks.forEach((link, index) =>{
-if (link.style.animation) {
-    link.style.animation = ""
-} else{
-link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.5}s`;
-
-}
-    });
-
-    //burger animation
-burger.classList.toggle("animate");
-
-        });
-}
-navSlide();
-
-//cookies alert
 /*
     GLOW COOKIES
     CREATED BY MANUEL CARRILLO
@@ -384,4 +348,3 @@ class LanguagesGC {
 }
 
 const glowCookies = new GlowCookies()
-
