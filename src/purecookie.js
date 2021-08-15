b2955fe299545a72cf553de3c24b43444109d36f
Gabriela85 <77332290+Gabriela85@users.noreply.github.com>
2021-08-15 22:20:21 +0200

    fix for firefox
    still without logo in mobil view :(

--- src/purecookie.js
+++ src/purecookie.js
@@ -6,7 +6,7 @@ var purecookieButton = "Aceptar cookies"; // Button text
 // ---        --- //
 
 
-function pureFadeIn(elem, display){
+function pureFadeIn(elem, display) {
   var el = document.getElementById(elem);
   el.style.opacity = 0;
   el.style.display = display || "block";
@@ -19,7 +19,7 @@ function pureFadeIn(elem, display){
     }
   })();
 };
-function pureFadeOut(elem){
+function pureFadeOut(elem) {
   var el = document.getElementById(elem);
   el.style.opacity = 1;
 
@@ -32,39 +32,44 @@ function pureFadeOut(elem){
   })();
 };
 
-function setCookie(name,value,days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
 }
 function eraseCookie(name) {
-    document.cookie = name+'=; Max-Age=-99999999;';
+  document.cookie = name + '=; Max-Age=-99999999;';
 }
 
 function cookieConsent() {
   if (!getCookie('purecookieDismiss')) {
     document.body.innerHTML += '<div class="cookieConsentContainer" id="cookieConsentContainer"><div class="cookieTitle"><a>' + purecookieTitle + '</a></div><div class="cookieDesc"><p>' + purecookieDesc + ' ' + purecookieLink + '</p></div><div class="cookieButton"><a onClick="purecookieDismiss();">' + purecookieButton + '</a></div></div>';
-	pureFadeIn("cookieConsentContainer");
+    pureFadeIn("cookieConsentContainer");
   }
 }
 
 function purecookieDismiss() {
-  setCookie('purecookieDismiss','1',7);
+  setCookie('purecookieDismiss', '1', 7);
   pureFadeOut("cookieConsentContainer");
 }
 
-window.onload = function() { cookieConsent(); };
+window.onload = function () { cookieConsent(); };
+
+setCookie('name', 'value', {
+  sameSite: 'none',
+  secure: true
+})
